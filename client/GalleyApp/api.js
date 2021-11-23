d4b02332f15a32a8af099c2c7bf766fc269a9caf
Angel <araozangel842@gmail.com>
2021-11-23 21:49:41 +0200

    Backend in Heroku, cleaning code and soon update styles

--- client/GalleyApp/api.js
+++ client/GalleyApp/api.js
@@ -1,43 +1,43 @@
-const API = 'http://10.0.2.2:3000/task'
+const API = 'https://todo-task-list-native.herokuapp.com/task';
+//const API = 'http://192.168.0.13:3000/task';
 export const getTasks = async () => {
-     const load = await fetch(API)
-    return await load.json()
+	console.log('getting tasks...');
+	const load = await fetch(API);
 
-}
-//localhost:3000
+	return await load.json();
+};
 
-export const getTask = async ( id ) => {
-const load = await fetch(`${API}/${id}`)
-return await load.json()
-}
+export const getTask = async (id) => {
+	const load = await fetch(`${API}/${id}`);
+	return await load.json();
+};
 
 export const saveTask = async (newTask) => {
-   const res =  await fetch(API, {
-         method: 'POST',
-         headers: { 
-             Accept: 'application/json', 
-             'Content-Type': 'application/json'
-                  },
-        body: JSON.stringify(newTask)
-                  })
-            return await res.json()
-}
+	console.log('clicking new task');
+	const res = await fetch(API, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(newTask),
+	});
+	return await res.json();
+};
 
 export const updateTask = async (id, task) => {
-    const res = await fetch(`${API}/${id}`, {
-        method: 'PUT',
-        headers: { 
-            Accept: 'application/json', 
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(task)
-    })
-    
-    return res
-}
+	const res = await fetch(`${API}/${id}`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(task),
+	});
 
-export const deleteTask = async ( id ) => {
-   await fetch(`${API}/${id}`, 
-    { method: "DELETE"}
-    )
-}
+	return res;
+};
+
+export const deleteTask = async (id) => {
+	await fetch(`${API}/${id}`, { method: 'DELETE' });
+};
--- client/GalleyApp/components/TaskList.js
+++ client/GalleyApp/components/TaskList.js
@@ -1,67 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { FlatList, RefreshControl} from 'react-native'
-import { useIsFocused } from '@react-navigation/native'
+import React, { useEffect, useState } from 'react';
+import { FlatList, RefreshControl } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 
-import TaskItem from './TaskItem'
+import TaskItem from './TaskItem';
 
-import { getTasks, deleteTask } from '../api'
+import { getTasks, deleteTask } from '../api';
 
 const TaskList = () => {
+	const [tasks, setTasks] = useState([]);
+	const [refreshing, setRefreshing] = useState(false);
 
-       const [ tasks, setTasks ] = useState([])
-       const [ refreshing, setRefreshing ] = useState(false)
+	const isFocused = useIsFocused();
 
-       const isFocused = useIsFocused()
+	const LoadTasks = async () => {
+		const data = await getTasks();
+		console.log(data);
+		setTasks(data);
+	};
+	useEffect(() => {
+		if (isFocused) {
+			LoadTasks();
+		}
+	}, [isFocused]);
 
-const LoadTasks = async () => {
-   const data= await getTasks()
-   console.log(data)
-   setTasks(data)
-      }     
-useEffect(() => {
-      if(isFocused){
-            LoadTasks()
-      }     
-}, [isFocused])
+	const handleDelete = async (id) => {
+		// console.log(id)
+		await deleteTask(id);
+		await LoadTasks();
+	};
 
-const handleDelete = async (id) => {
-      // console.log(id)
-      await  deleteTask(id)
-      await LoadTasks()
-}
-
-const renderItem= ({item}) => {
-            return   <TaskItem task={item} handleDelete={handleDelete}/>
-}
-//Usando Callback de React
-const onRefresh = React.useCallback(
-      async() => {
-             setRefreshing(true)
-             await LoadTasks()
-            setRefreshing(false)
-      
-      }
-)
-return (
-           <FlatList 
-           style={{width: '100%'}}
-               data={tasks}
-               keyExtractor={(item) => item.id + ''}
-               renderItem={ renderItem }
-               refreshControl={
-                     <RefreshControl
-                     refreshing={refreshing}
-                      progressBackgroundColor= "#2a2a72"
-                      progressTextColor= "#ffffff"
-                        onRefresh={onRefresh}
-                      />
-                   }  
-                      
-                 /> 
-                 )
-                              
-                     
-                                  
-     
-  }    
-export default TaskList
+	const renderItem = ({ item }) => {
+		return <TaskItem task={item} handleDelete={handleDelete} />;
+	};
+	//Usando Callback de React
+	const onRefresh = React.useCallback(async () => {
+		setRefreshing(true);
+		await LoadTasks();
+		setRefreshing(false);
+	});
+	return (
+		<FlatList
+			style={{ width: '100%' }}
+			data={tasks}
+			keyExtractor={(item) => item.id + ''}
+			renderItem={renderItem}
+			refreshControl={
+				<RefreshControl
+					refreshing={refreshing}
+					progressBackgroundColor="#2a2a72"
+					progressTextColor="#ffffff"
+					onRefresh={onRefresh}
+				/>
+			}
+		/>
+	);
+};
+export default TaskList;
